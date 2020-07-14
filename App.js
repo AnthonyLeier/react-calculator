95792a1937dc9b150cae78d9959c23747dac9543
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-14 14:22:38 -0300

    Correção de Bugs

--- App.js
+++ App.js
@@ -15,9 +15,8 @@ export default class App extends Component {
 	state = {...initialState};
 
 	addDigit = (n) => {
-		if (n === '.' && this.state.displayValue.includes('.')) return;
-		const clearDisplay =
-			this.state.displayValue === '0' || this.state.clearDisplay;
+		const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+		if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) return;
 		const currentValue = clearDisplay ? '' : this.state.displayValue;
 		const displayValue = currentValue + n;
 		this.setState({displayValue, clearDisplay: false});
@@ -50,7 +49,7 @@ export default class App extends Component {
 
 			values[1] = 0;
 			this.setState({
-				displayValue: values[0],
+				displayValue: `${values[0]}`,
 				operation: equals ? null : operation,
 				current: equals ? null : 1,
 				clearDisplay: true,
