03c0c5d3cc06c7aef457c8dd4cdf513145a37606
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-14 14:30:52 -0300

    Dark Theme

--- src/components/Display.js
+++ src/components/Display.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 20,
 		justifyContent: 'center',
-		backgroundColor: 'rgba(0,0,0,0.6)',
+		backgroundColor: '#121212', //'rgba(0,0,0,0.6)',
 		alignItems: 'flex-end',
 	},
 	info: {
